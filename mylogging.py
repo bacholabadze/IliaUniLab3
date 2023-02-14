55aa68e88f1e1506047a215d6e3af62465f39809
ambri <bachukilabadze@gmail.com>
2023-02-14 21:31:28 +0400

    update logging with sys arguments

--- mylogging.py
+++ mylogging.py
@@ -1,15 +1,20 @@
 import logging
+import sys
 import inspect
 
-# line = lambda :
-
-# logging.basicConfig(level= logging.DEBUG)
-logging.basicConfig(filename='./app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s', level= logging.DEBUG)
+try:
+    print("Shesadzlebelia kodis gashveba =>>>> python mylogging.py log_name.log a")
+    print(f"Gadacemuli Argumentebia: {sys.argv[1:]}")
+    log_file_name = str(sys.argv[1])
+    log_file_mode = str(sys.argv[2])
+    # logging.basicConfig(level= logging.DEBUG)
+    logging.basicConfig(filename=log_file_name, filemode=log_file_mode, format='%(name)s - %(levelname)s - %(message)s', level= logging.DEBUG)
+except:
+    logging.basicConfig(filename="app.log", filemode="w", format='%(name)s - %(levelname)s - %(message)s',
+                        level=logging.DEBUG)
 
 logging.debug('This will get logged')
 logging.warning('This is warning message')
 logging.error('This is error message')
 logging.critical('This is critical message')
 logging.info(f'This is info message. Line NO: {inspect.currentframe().f_lineno}')
-
-# print(line)
